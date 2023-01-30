1f94598d87daf314b963c0175a2fe4d1c41c2470
SergY29 <yurtin.sergey.v@gmail.com>
2023-01-30 12:33:07 +0200

    add pages

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,35 +1,20 @@
+import { lazy } from 'react';
 import { Route, Routes } from 'react-router-dom';
-import { Layout } from './Layout';
+import { Layout } from 'components/Layout';
+const HomePage = lazy(() => import('pages/HomePage'));
+const ContactsPage = lazy(() => import('pages/ContactsPage'));
+const LoginPage = lazy(() => import('pages/LoginPage'));
+const RegisterPage = lazy(() => import('pages/RegisterPage'));
 
 export const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<Layout />}></Route>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<HomePage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/contacts" element={<ContactsPage />} />
+      </Route>
     </Routes>
   );
 };
-
-// import { lazy } from 'react';
-// import { Route, Routes } from 'react-router-dom';
-
-// import { Layout } from './Layout';
-// const Home = lazy(() => import('../pages/Home'));
-// const Movies = lazy(() => import('../pages/Movies'));
-// const MovieDetails = lazy(() => import('./MovieDetails'));
-// const Cast = lazy(() => import('../components/Cast'));
-// const Reviews = lazy(() => import('../components/Reviews'));
-
-// export const App = () => {
-//   return (
-//     <Routes>
-//       <Route path="/" element={<Layout />}>
-//         <Route index element={<Home />} />
-//         <Route path="movies" element={<Movies />} />
-//         <Route path="movies/:id" element={<MovieDetails />}>
-//           <Route path="cast" element={<Cast />} />
-//           <Route path="reviews" element={<Reviews />} />
-//         </Route>
-//       </Route>
-//     </Routes>
-//   );
-// };
---
+++ src/pages/ContactsPage.jsx
@@ -0,0 +1,7 @@
+export default function ContactsPage() {
+  return (
+    <div>
+      <h1>ContactsPage</h1>
+    </div>
+  );
+}
