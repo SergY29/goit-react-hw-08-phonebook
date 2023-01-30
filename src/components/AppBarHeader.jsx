618be577af18315444cc7ff7fb77f0ef936819a6
SergY29 <yurtin.sergey.v@gmail.com>
2023-01-30 16:23:33 +0200

    appbar

--- src/components/AppBarHeader.jsx
+++ src/components/AppBarHeader.jsx
@@ -3,29 +3,55 @@ import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
+import Tab from '@mui/material/Tab';
+import Tabs from '@mui/material/Tabs';
+import { Link } from 'react-router-dom';
+import { useState } from 'react';
+
+function a11yProps(index) {
+  return {
+    id: `simple-tab-${index}`,
+    'aria-controls': `simple-tabpanel-${index}`,
+  };
+}
 
 export const AppBarHeader = () => {
+  const [value, setValue] = useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
-        <Toolbar>
-          <Box sx={{ display: { xs: 'none', md: 'flex' }, flexGrow: 1 }}>
-            <Typography variant="h6" component="div" sx={{ mr: 2 }}>
-              News
-            </Typography>
-            <Typography variant="h6" component="div" sx={{ mr: 2 }}>
-              Newssss
-            </Typography>
-            <Typography variant="h6" component="div" sx={{ mr: 2 }}>
-              Newssssqwewqeqs
-            </Typography>
-          </Box>
+      <AppBar position="static" color="secondary">
+        <Toolbar component="nav">
+          <Tabs
+            sx={{ display: { xs: 'none', md: 'flex' }, flexGrow: 1 }}
+            value={value}
+            onChange={handleChange}
+            aria-label="basic tabs example"
+          >
+            <Tab label="Home" to="/" component={Link} />
+            <Tab
+              label="Register"
+              to="/register"
+              component={Link}
+              {...a11yProps(0)}
+            />
+            <Tab label="Login" to="/login" component={Link} {...a11yProps(1)} />
+            <Tab
+              label="Contacts"
+              to="/contacts"
+              component={Link}
+              {...a11yProps(2)}
+            />
+          </Tabs>
           <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
             <Typography variant="h6" component="div" sx={{ mr: 2 }}>
               Newsddd
             </Typography>
             <Button variant="contained" color="secondary">
-              {' '}
               Logout
             </Button>
           </Box>
@@ -34,3 +60,76 @@ export const AppBarHeader = () => {
     </Box>
   );
 };
+
+// import * as React from 'react';
+// import PropTypes from 'prop-types';
+// import Tabs from '@mui/material/Tabs';
+// import Tab from '@mui/material/Tab';
+// import Typography from '@mui/material/Typography';
+// import Box from '@mui/material/Box';
+
+// function TabPanel(props) {
+//   const { children, value, index, ...other } = props;
+
+//   return (
+//     <div
+//       role="tabpanel"
+//       hidden={value !== index}
+//       id={`simple-tabpanel-${index}`}
+//       aria-labelledby={`simple-tab-${index}`}
+//       {...other}
+//     >
+//       {value === index && (
+//         <Box sx={{ p: 3 }}>
+//           <Typography>{children}</Typography>
+//         </Box>
+//       )}
+//     </div>
+//   );
+// }
+
+// TabPanel.propTypes = {
+//   children: PropTypes.node,
+//   index: PropTypes.number.isRequired,
+//   value: PropTypes.number.isRequired,
+// };
+
+// function a11yProps(index) {
+//   return {
+//     id: `simple-tab-${index}`,
+//     'aria-controls': `simple-tabpanel-${index}`,
+//   };
+// }
+
+// export default function BasicTabs() {
+//   const [value, setValue] = React.useState(0);
+
+//   const handleChange = (event, newValue) => {
+//     setValue(newValue);
+//   };
+
+//   return (
+//     <Box sx={{ width: '100%' }}>
+//       <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+//         <Tabs
+//           value={value}
+//           onChange={handleChange}
+//           aria-label="basic tabs example"
+//         >
+//           <Tab label="Item One" {...a11yProps(0)} />
+//           <Tab label="Item Two" {...a11yProps(1)} />
+//           <Tab label="Item Three" {...a11yProps(2)} />
+//         </Tabs>
+//       </Box>
+//       <TabPanel value={value} index={0}>
+//         Item One
+//       </TabPanel>
+//       <TabPanel value={value} index={1}>
+//         Item Two
+//       </TabPanel>
+//       <TabPanel value={value} index={2}>
+//         Item Three
+//       </TabPanel>
+//     </Box>
+//   );
+// }
--- src/index.js
+++ src/index.js
@@ -3,11 +3,33 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { App } from 'components/App';
 import './index.css';
+import { ThemeProvider } from '@emotion/react';
+import { createTheme } from '@mui/material/styles';
+
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      light: '#778ce8',
+      main: '#3f50b5',
+      dark: '#002884',
+      contrastText: '#757ce8',
+    },
+    secondary: {
+      light: '#ff7961',
+      main: '#f44336',
+      dark: '#ba000d',
+      contrastText: '#000',
+    },
+  },
+});
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter basename="/goit-react-hw-08-phonebook">
-      <App />
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
