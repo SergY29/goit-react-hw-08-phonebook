d321069263d5b359f7a2e7e8fdc1771efff75821
SergY29 <yurtin.sergey.v@gmail.com>
2023-01-30 21:24:29 +0200

    Update Navigation.jsx

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -5,22 +5,10 @@ import { Link } from 'react-router-dom';
 import { useState } from 'react';
 
 export const Navigation = () => {
-  const [value, setValue] = useState(0);
-
-  const handleChange = (_, newValue) => {
-    setValue(newValue);
-  };
-
   return (
     <Toolbar component="nav" sx={{ flexGrow: 1 }}>
-      <Tabs
-        value={value}
-        onChange={handleChange}
-        aria-label="basic tabs example"
-      >
-        <Tab label="Home" to="/" component={Link} />
-        <Tab label="Contacts" to="/contacts" component={Link} />
-      </Tabs>
+      <Tab label="Home" to="/" component={Link} />
+      <Tab label="Contacts" to="/contacts" component={Link} />
     </Toolbar>
   );
 };
