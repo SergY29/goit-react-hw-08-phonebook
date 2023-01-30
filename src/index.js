d66f8e66185fcb9ab43845062a7b63881450f04f
SergY29 <yurtin.sergey.v@gmail.com>
2023-01-30 16:41:23 +0200

    fixed color

--- src/index.js
+++ src/index.js
@@ -11,17 +11,18 @@ const theme = createTheme({
   palette: {
     primary: {
       light: '#778ce8',
-      main: '#3f50b5',
+      main: '#2a3eb1',
       dark: '#002884',
-      contrastText: '#757ce8',
+      contrastText: '#fff',
     },
     secondary: {
       light: '#ff7961',
-      main: '#f44336',
+      main: '#ffc400',
       dark: '#ba000d',
       contrastText: '#000',
     },
   },
+
 });
 
 ReactDOM.createRoot(document.getElementById('root')).render(
