71382e129c08164c51532cdbccc8b952518e889a
SergY29 <yurtin.sergey.v@gmail.com>
2023-01-30 21:27:49 +0200

    update navigation

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,14 +1,17 @@
+import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+
 import Toolbar from '@mui/material/Toolbar';
 import Tab from '@mui/material/Tab';
-import Tabs from '@mui/material/Tabs';
-import { Link } from 'react-router-dom';
-import { useState } from 'react';
+
+import { selectIsLoggedIn } from 'redux/authUser/selectors';
 
 export const Navigation = () => {
+  const isLoggedIn = useSelector(selectIsLoggedIn);
   return (
     <Toolbar component="nav" sx={{ flexGrow: 1 }}>
       <Tab label="Home" to="/" component={Link} />
-      <Tab label="Contacts" to="/contacts" component={Link} />
+      {isLoggedIn && <Tab label="Contacts" to="/contacts" component={Link} />}
     </Toolbar>
   );
 };
