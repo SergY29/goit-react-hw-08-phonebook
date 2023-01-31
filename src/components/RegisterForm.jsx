f32df8b57c31d0963c9b04a33ce21ae970a96a70
SergY29 <yurtin.sergey.v@gmail.com>
2023-01-31 14:57:37 +0200

    work 6

--- src/components/RegisterForm.jsx
+++ src/components/RegisterForm.jsx
@@ -1,13 +1,21 @@
-import { useDispatch } from 'react-redux';
+// import { useDispatch } from 'react-redux';
 // import { register } from 'redux/auth/operations';
-import { FormControl } from '@mui/material';
+import { FormControl, InputLabel, Input } from '@mui/material';
+import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
 
 export const RegisterForm = () => {
-  const dispatch = useDispatch();
+  //   const dispatch = useDispatch();
 
   const handleSubmit = e => {
     e.preventDefault();
     const form = e.currentTarget;
+    console.log(
+      'отправка',
+      form.elements.name.value,
+      form.elements.email.value,
+      form.elements.password.value
+    );
     // dispatch(
     //   register({
     //     name: form.elements.name.value,
@@ -19,20 +27,32 @@ export const RegisterForm = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit} autoComplete="off">
-      <label>
-        Username
-        <input type="text" name="name" />
-      </label>
-      <label>
-        Email
-        <input type="email" name="email" />
-      </label>
-      <label>
-        Password
-        <input type="password" name="password" autoComplete="on" />
-      </label>
-      <button type="submit">Register</button>
-    </form>
+    <Box
+      component="form"
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        '& > :not(style)': { m: 1, width: '25ch' },
+      }}
+      noValidate
+      autoComplete="off"
+      onSubmit={handleSubmit}
+    >
+      <FormControl>
+        <InputLabel htmlFor="my-input">Username</InputLabel>
+        <Input id="my-input" type="text" name="name" />
+      </FormControl>
+      <FormControl>
+        <InputLabel htmlFor="my-email">Email</InputLabel>
+        <Input id="my-emai" type="email" name="email" />
+      </FormControl>
+      <FormControl>
+        <InputLabel htmlFor="my-input">Password</InputLabel>
+        <Input id="my-passwor" name="password" autoComplete="on" />
+      </FormControl>
+      <Button variant="outlined" type="submit">
+        Register
+      </Button>
+    </Box>
   );
 };
